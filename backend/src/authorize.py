4f1be6bcd4671e59eac43d2dd75433ed88d86e29
Kerry Mahne <kerry.mahne@tretton37.com>
2020-06-26 10:21:04 +0200

    .env is now read on backend

--- backend/src/authorize.py
+++ backend/src/authorize.py
@@ -2,8 +2,7 @@ from google.oauth2 import id_token
 from google.auth.transport import requests
 import os
 
-name = os.getenv("GCLIENT")
-print("the", name)
+gclientId = os.getenv("GCLIENT")
 
 def authorizer(token):
     print("tjolken", token)
--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 from pathlib import Path  # Python 3.6+ only
 
-env_path = Path('../')
-print("peff", env_path)
+env_path = Path('..') / '.env'
 
 load_dotenv(dotenv_path = env_path)
